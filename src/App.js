9006c44044f960996cd6e1e1a0973958e6088cba
Piotr Szczesniak <piotr.szczesniak@trafiony.com>
2022-05-20 09:02:49 +0200

    make accordion finally work as expected

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
 import { React } from 'react';
-import { useState } from 'react';
+import { Accordion } from './components/Accordion';
 
 function App() {
-  const accordionData = [
-    {
-      title: 'Section 1',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-      laborum cupiditate possimus labore, hic temporibus velit dicta earum
-      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-      voluptatem.`,
-    },
-    {
-      title: 'Section 2',
-      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
-      reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
-      quaerat iure quos dolorum accusantium ducimus in illum vero commodi
-      pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
-      quidem maiores doloremque est numquam praesentium eos voluptatem amet!
-      Repudiandae, mollitia id reprehenderit a ab odit!`,
-    },
-    {
-      title: 'Section 3',
-      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-      dolor ut sequi minus iste? Quas?`,
-    },
-  ];
-
-  const [selected, setSelected] = useState(null);
-
-  function setActive(index) {
-    if (selected === index) {
-      console.log('equal');
-    }
-  }
-
   return (
-    <div className='App'>
-      <h3>Current selection: {selected}</h3>
-      {accordionData.map((item, index) => (
-        <div onClick={() => setActive(index)} key={index}>
-          <h3>
-            {item.title} index: {index}
-          </h3>
-          <p>{item.content}</p>
-        </div>
-      ))}
+    <div className="App">
+      <Accordion />
     </div>
   );
 }
---
+++ src/components/Accordion.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Accordion = () => {
+  const [opened, setOpened] = useState(0);
+
+  function setActive(index) {
+    if (opened === index) {
+      setOpened(null);
+    } else {
+      setOpened(index);
+    }
+  }
+  return (
+    <div>
+      {accordionData.map((item, index) => (
+        <div onClick={() => setActive(index)} key={index}>
+          <h3>
+            {item.title} {opened === index ? '-' : '+'}
+          </h3>
+          <p>{item.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const accordionData = [
+  {
+    title: 'Section 1',
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+    laborum cupiditate possimus labore, hic temporibus velit dicta earum
+    suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+    voluptatem.`,
+  },
+  {
+    title: 'Section 2',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+    Repudiandae, mollitia id reprehenderit a ab odit!`,
+  },
+  {
+    title: 'Section 3',
+    content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
+    quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
+    dolor ut sequi minus iste? Quas?`,
+  },
+];
+
+export { Accordion };
